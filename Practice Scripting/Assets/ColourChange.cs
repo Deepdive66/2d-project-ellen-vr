a8b8b46f7c8d20148ddebbef21f51c204326aa1f
Deepti  Belsare <deeptibelsare@Deeptis-MacBook-Air.local>
2018-08-26 15:11:24 -0400

    First commit

---
+++ Practice Scripting/Assets/ColourChange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+
+public class ColourChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.R)){
+            gameObject.renderer
+         // gameObject.GetComponent<Renderer>().material.color("Red");
+        }
+        if(Input.GetKeyDown(KeyCode.G)){
+           //gameObject.GetComponent<Renderer>().material.color("Green");
+        }
+        if(Input.GetKeyDown(KeyCode.B)){
+          // gameObject.GetComponent<Renderer>().material.color("Blue");
+        }
+	}
+}
